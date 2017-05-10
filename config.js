265c920a26332dde26774b2da2f97f7ba6d4c2a0
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2017-05-10 10:34:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,3 @@ module.exports = {
 
 
 
-//https://graph.facebook.com/v2.6/1409237165800151?fields=first_name&access_token=EAAbPsO8J2DABAB44KWaDmoR3EJmzP0ZCGwW6PrQWRDPAHmvACK9AiHKnPRBlBptk52WzutiPfmRXph9tI3gILx1fY9YkjN6wdEZBgVEr3hQI256uMpIBdNlOWlLYoS2vr6GVT1ZARxl7OenSpdZCzgaVF1foxfhJE1lkb5sZARAZDZD
-//https://graph.facebook.com/v2.6/1409237165800151?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAbPsO8J2DABAB44KWaDmoR3EJmzP0ZCGwW6PrQWRDPAHmvACK9AiHKnPRBlBptk52WzutiPfmRXph9tI3gILx1fY9YkjN6wdEZBgVEr3hQI256uMpIBdNlOWlLYoS2vr6GVT1ZARxl7OenSpdZCzgaVF1foxfhJE1lkb5sZARAZDZD

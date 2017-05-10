e483638026897a5991a9e2c2fcf65ad64a6bfba2
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2017-05-10 10:33:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /** Config */
 
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAbPsO8J2DABAB44KWaDmoR3EJmzP0ZCGwW6PrQWRDPAHmvACK9AiHKnPRBlBptk52WzutiPfmRXph9tI3gILx1fY9YkjN6wdEZBgVEr3hQI256uMpIBdNlOWlLYoS2vr6GVT1ZARxl7OenSpdZCzgaVF1foxfhJE1lkb5sZARAZDZD',
-    FB_PAGE_ID: '418798498494879',
-    FB_VERIFY_TOKEN: 'dpchn',
-    WIT_TOKEN: 'ETBFKBSVYYPL74GNBYPVK4DATXM5JNLG',
+    FB_PAGE_TOKEN: 'FB_PAGE_TOKEN',//From facebook page
+    FB_PAGE_ID: 'FB_PAGE_ID',//from facebook page
+    FB_VERIFY_TOKEN: 'FB_VERIFY_TOKEN',//It;s up to you, you can keep secreate key as you wish
+    WIT_TOKEN: 'WIT_TOKEN',// get from wit.ai
 };
 
 
